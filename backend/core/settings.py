0e43357eb639ad3ac873b857efe4a6f9bdac8807
hackdumping <erictchoupe5@gmail.com>
2026-04-24 19:04:16 +0100

    amelioration de l'app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
 WHITENOISE_USE_FINDERS = True
 
