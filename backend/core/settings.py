744611b2f9c179b207964b2b31adfbc7b8f35fc2
hackdumping <erictchoupe5@gmail.com>
2026-04-24 19:47:01 +0100

    amelioration de l'app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -95,6 +95,18 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "x-tenant-id",
+]
 
 ROOT_URLCONF = 'core.urls'
 
